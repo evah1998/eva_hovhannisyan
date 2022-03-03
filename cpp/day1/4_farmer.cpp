623ee70a76b62248caeec164857ada21e1fed9b9
eva <evahovhannisyan8@gmail.com>
2022-03-04 00:47:35 +0400

    Homework 4

---
+++ cpp/day1/4_farmer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int chickens = 1;
+        int cows = 2;
+        int pigs = 3;
+	int sum_legs = chickens * 2 + cows * 4 + pigs * 4;
+	cout << sum_legs << "\n";
+
+}
