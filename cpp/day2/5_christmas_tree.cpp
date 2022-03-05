99dc283a46a238e741752a5809172984a08ab927
eva <evahovhannisyan8@gmail.com>
2022-03-06 00:14:17 +0400

    Homework 5

---
+++ cpp/day2/5_christmas_tree.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Input number: ";
+	cin >> n;
+	const int num = n;
+
+/* First christas tree
+ *****
+ ****
+ ***
+ **
+ *   */
+	int i = num;	
+	while (i) {
+		int j = i;
+		while (j) {
+			cout << "*";
+			j--;
+		}
+		cout << "\n";
+		i--;
+	}
+	cout << "\n";
+
+/*Second christas tree
+     *
+    **
+   ***
+  ****
+ *****   */
+
+	i = 1;
+	while (i <= num) {
+		int j = 1;
+		while (j <= num - i) {
+			cout << " ";
+			j++;
+		}
+		j = 1;
+		while (j <= i) {
+			cout << "*";
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	cout << "\n";
+
+/*Third christas tree
+ *
+ **
+ ***
+ ****
+ *****   */
+
+	i = 1;
+	while (i <= num) {
+		int j = 1;
+		while (j <= i) {
+			cout << "*";
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	cout << "\n";
+
+/*Fourth christmas tree
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *     */
+	i = 1;
+        while (i <= num) {
+                int j = 1;
+                while (j <= num - i) {
+                        cout << " ";
+                        j++;
+                }
+                j = 1;
+                while (j <= i) {
+                        cout << "* ";
+                        j++;
+                }
+                cout << "\n";
+                i++;
+        }
+        cout << "\n";
+	return 0;
+}
