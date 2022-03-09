52149f0edc9a13cf60ea380d86bb312840396847
eva <evahovhannisyan8@gmail.com>
2022-03-09 23:28:07 +0400

    Homework 5

---
+++ cpp/day4/5_in_reverse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void Array(int);
+void printArray(int,int);
+int main() {
+	int n;
+	cout << "Size of array: ";
+	cin >> n;
+	const int size = n;
+	
+	int arr[size];
+	srand(time(NULL));
+	for (int i = 1; i <= n; i++) {
+		arr[i] = rand() % 100;
+		cout << arr[i] << ", ";
+	}
+	cout << endl << endl;
+	for (int i = n; i > 0; i--)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+	return 0;
+}
+
