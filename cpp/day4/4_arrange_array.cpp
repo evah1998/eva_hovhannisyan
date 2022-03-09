b1d96b5ab0b9fd0662a8ccd66af4d5271321be5e
eva <evahovhannisyan8@gmail.com>
2022-03-09 21:44:15 +0400

    Homework 4

---
+++ cpp/day4/4_arrange_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	int arr[10];
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 100;
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9-i; j++) {
+			if (arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp; 
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+	return 0;
+}
+
