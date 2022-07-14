279ee4254738e19395c06e575f99181a60333abf
eva <evahovhannisyan8@gmail.com>
2022-07-14 08:57:05 +0400

    Homework nodejs

---
+++ web/nodejs/day1/test.js
@@ -0,0 +1,22 @@
+import express from "express"
+import path from "path"
+
+const app = express()
+const port = 3000
+
+app.use((req, res, next) => {
+    console.log(req.url);
+    next()
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve("public/index.html"))
+})
+
+app.get('/user', (req, res) => {
+    res.send("Hello user")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
